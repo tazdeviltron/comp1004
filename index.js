ea7467af4391311a6c3dd5cc196f517c4b38ad59
tazdeviltron <149820174+tazdeviltron@users.noreply.github.com>
2024-03-20 19:33:26 +0000

    stuff

--- index.js
+++ index.js
@@ -9,7 +9,64 @@ const image = new Image();
 image.src = "zombiemap.png";
 const playerImage = new Image();
 playerImage.src = "Spritesheet.png";
-image.onload = () => {
-    c.drawImage(image, -850, -200)
-    c.drawImage(playerImage, canvas.width / 2 - playerImage.width / 2, canvas.height / 2 - playerImage.height / 2)
+class Sprint {
+    constructor({ position, velocity,image}) {
+        this.position = position
+        this.image = image
+    }
+    draw() {
+        c.drawImage(this.image, -200, -100)
+    }
 }
+const background = new Sprint({
+    position: {
+        x = -200,
+        y= -100
+    },
+    image:image
+})
+const keys = {
+    w: {
+        pressed:false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+    function animate() {
+        window.requestAnimationFrame(animate)
+      background.draw()
+        c.drawImage(playerImage,
+            0,
+            0,
+            playerImage.width / 4,
+            playerImage.height / 4,
+            canvas.width / 2,
+            canvas.height / 2,
+            playerImage.width / 4,
+            playerImage.height / 4,
+        )
+       // if () {
+
+       // }
+    }
+    animate()
+    window.addEventListener("keydown", (e) => {
+        switch (e.key) {
+            case "w":
+                break;
+            case "s":
+                break;
+            case "a":
+                break;
+            case "d":
+                break;
+        }
+    })
+
